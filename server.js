955b8f295bf199859c8ef797204ab46f467aabcc
Joe Yuan <joe.yuan19@gmail.com>
2013-04-07 02:10:54 -0400

    update

--- server.js
+++ server.js
@@ -1,9 +1,21 @@
 var io = require('socket.io').listen(8080);
 
 io.sockets.on('connection', function (socket) {
-  socket.join('test');
-  var clients = io.sockets.clients('test');
-  if ( clients.length === 2 ) {
+  var len_clients = io.sockets.clients('test').length;
+  if (len_clients < 2) {
+    socket.join('test');
+    len_clients += 1;
+  }
+  console.log('number clients: ' + len_clients + '\n');  
+  if ( len_clients === 2 ) {
+    console.log('sending start signal');
+    var count = 0;
     io.sockets.in('test').emit('go');
   }
+  socket.on('sendDataToServer', function (data) {
+    console.log('data received\n');
+    count += 1;
+    console.log('number data received: ' + count + '\n');
+    socket.broadcast.in('test').emit('returnDataToClient',data);
+  });
 });
